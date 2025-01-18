0107d0f68645836771889a3694428a24b8987257
nishie yuuto <yuutonishie@gmail.com>
2025-01-19 05:59:59 +0900

    引き算の機能を追加した。

--- app.py
+++ app.py
@@ -11,5 +11,14 @@ if st.button('計算の問題出題(足し算～＋～)'):
     st.session_state.bb=ran.randint(1,100)
 st.write(st.session_state.aa,'+',st.session_state.bb)
 if st.button('こたえ'):
-    st.session_state.ancer=st.session_state.aa+st.session_state.bb
-st.write(st.session_state.ancer)
+    st.session_state.ancera=st.session_state.aa+st.session_state.bb
+st.write(st.session_state.ancera)
+
+if st.button('計算の問題出題(足し算～ー～)'):
+    
+    st.session_state.cc=ran.randint(1,100)
+    st.session_state.dd=ran.randint(1,100)
+st.write(st.session_state.cc,'-',st.session_state.dd)
+if st.button('こたえ'):
+    st.session_state.ancerb=st.session_state.cc-st.session_state.dd
+st.write(st.session_state.ancerb)
