5e2ac0cd7e88c4ebacf4a8bfca305736dfe144a6
nishie yuuto <yuutonishie@gmail.com>
2025-01-18 12:27:31 +0900

    答えボタンの機能を追加した。＋答えもボタンを押すと計算が消えたので、計算と答えがずっと表示されるように変えた。

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import random as ran
 st.title('計算＆漢字クイズアプリ')
 #プログラム↓
 if st.button('計算の問題出題(足し算～＋～)'):
-    aa=ran.randint(1,100)
-    bb=ran.randint(1,100)
-    st.write(aa,'+',bb)
-    if st.button('こたえ'):
-        ancer=aa+bb
-        st.write(ancer)
+    
+    st.session_state.aa=ran.randint(1,100)
+    st.session_state.bb=ran.randint(1,100)
+st.write(st.session_state.aa,'+',st.session_state.bb)
+if st.button('こたえ'):
+    st.session_state.ancer=st.session_state.aa+st.session_state.bb
+st.write(st.session_state.ancer)
