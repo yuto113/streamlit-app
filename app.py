5824b226334e9563efbf6b183492a441c6be70f8
nishie yuuto <yuutonishie@gmail.com>
2025-01-18 11:47:08 +0900

    足し算の計算の問題出題機能を追加した

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import random as ran
 # 作るもの：＠計算＆漢字クイズアプリ＠
 
 # タイトルをつける。
 st.title('計算＆漢字クイズアプリ')
 #プログラム↓
+if st.button('計算の問題出題(足し算～＋～)'):
+    aa=ran.randint(1,100)
+    bb=ran.randint(1,100)
+    st.write(aa,'+',bb)
