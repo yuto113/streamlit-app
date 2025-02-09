ece47ad7ddbbc3b1cbc900f9a4e22bfe1810e725
yuto113 <yuutonishie@gmail.com>
2025-02-09 11:27:31 +0900

    掛け算機能を追加

--- app.py
+++ app.py
@@ -4,21 +4,48 @@ import random as ran
 
 # タイトルをつける。
 st.title('計算＆漢字クイズアプリ')
-#プログラム↓
+
+# 計算の問題出題(足し算～＋～)
+if 'aa' not in st.session_state:
+    st.session_state.aa = ran.randint(1, 100)
+    st.session_state.bb = ran.randint(1, 100)
+
 if st.button('計算の問題出題(足し算～＋～)'):
-    
-    st.session_state.aa=ran.randint(1,100)
-    st.session_state.bb=ran.randint(1,100)
-st.write(st.session_state.aa,'+',st.session_state.bb)
-if st.button('こたえ'):
-    st.session_state.ancera=st.session_state.aa+st.session_state.bb
-st.write(st.session_state.ancera)
+    st.session_state.aa = ran.randint(1, 100)
+    st.session_state.bb = ran.randint(1, 100)
+
+st.write(st.session_state.aa, '+', st.session_state.bb)
+
+if st.button('こたえ (足し算)'):
+    st.session_state.ancera = st.session_state.aa + st.session_state.bb
+    st.write(st.session_state.ancera)
+
+# 計算の問題出題(引き算～ー～)
+if 'cc' not in st.session_state:
+    st.session_state.cc = ran.randint(1, 100)
+    st.session_state.dd = ran.randint(1, 100)
 
 if st.button('計算の問題出題(引き算～ー～)'):
-    
-    st.session_state.cc=ran.randint(1,100)
-    st.session_state.dd=ran.randint(1,100)
-st.write(st.session_state.cc,'-',st.session_state.dd)
-if st.button('こたえ'):
-    st.session_state.ancerb=st.session_state.cc-st.session_state.dd
-st.write(st.session_state.ancerb)
+    st.session_state.cc = ran.randint(1, 100)
+    st.session_state.dd = ran.randint(1, 100)
+
+st.write(st.session_state.cc, '-', st.session_state.dd)
+
+if st.button('こたえ (引き算)'):
+    st.session_state.ancerb = st.session_state.cc - st.session_state.dd
+    st.write(st.session_state.ancerb)
+
+# 計算の問題出題(掛け算～✖～)
+if 'ee' not in st.session_state:
+    st.session_state.ee = ran.randint(1, 100)
+    st.session_state.ff = ran.randint(1, 100)
+
+if st.button('計算の問題出題(掛け算～✖～)'):
+    st.session_state.ee = ran.randint(1, 100)
+    st.session_state.ff = ran.randint(1, 100)
+
+st.write(st.session_state.ee, '✖', st.session_state.ff)
+
+if st.button('こたえ (掛け算)'):
+    st.session_state.ancerc = st.session_state.ee * st.session_state.ff
+    st.write(st.session_state.ancerc)
