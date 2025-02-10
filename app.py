3b74ba42889d3b1b8544909292f32434623ab353
yuto113 <yuutonishie@gmail.com>
2025-02-10 15:57:28 +0900

    引き算のアップデート

--- app.py
+++ app.py
@@ -35,47 +35,62 @@ if st.button('こたえ (繰り上がりのある足し算)'):
     st.session_state.ancerb = st.session_state.cc + st.session_state.dd
     st.write(st.session_state.ancerb)
 
-# 計算の問題出題(引き算～ー～)
+# 計算の問題出題(繰り下がりなしの引き算～ー～)
 if 'ee' not in st.session_state:
     st.session_state.ee = ran.randint(1, 100)
     st.session_state.ff = ran.randint(1, 100)
 
-if st.button('計算の問題出題(引き算～ー～)'):
+if st.button('計算の問題出題(繰り下がりなしの引き算～ー～)'):
     st.session_state.ee = ran.randint(1, 100)
     st.session_state.ff = ran.randint(1, 100)
 
 st.write(max(st.session_state.ee, st.session_state.ff), '-', min(st.session_state.ee, st.session_state.ff))
 
-if st.button('こたえ (引き算)'):
+if st.button('こたえ (繰り下がりなしの引き算)'):
     st.session_state.ancerc = abs(st.session_state.ee - st.session_state.ff)
     st.write(st.session_state.ancerc)
 
-# 計算の問題出題(掛け算～✖～)
+# 計算の問題出題(繰り下がりのある引き算～ー～)
 if 'gg' not in st.session_state:
-    st.session_state.gg = ran.randint(1, 100)
-    st.session_state.hh = ran.randint(1, 100)
+    st.session_state.gg = ran.randint(10, 99)
+    st.session_state.hh = ran.randint(10, 99)
 
-if st.button('計算の問題出題(掛け算～✖～)'):
-    st.session_state.gg = ran.randint(1, 100)
-    st.session_state.hh = ran.randint(1, 100)
+if st.button('計算の問題出題(繰り下がりのある引き算～ー～)'):
+    st.session_state.gg = ran.randint(10, 99)
+    st.session_state.hh = ran.randint(10, 99)
 
-st.write(st.session_state.gg, '✖', st.session_state.hh)
+st.write(max(st.session_state.gg, st.session_state.hh), '-', min(st.session_state.gg, st.session_state.hh))
 
-if st.button('こたえ (掛け算)'):
-    st.session_state.ancerd = st.session_state.gg * st.session_state.hh
+if st.button('こたえ (繰り下がりのある引き算)'):
+    st.session_state.ancerd = abs(st.session_state.gg - st.session_state.hh)
     st.write(st.session_state.ancerd)
 
-# 計算の問題出題(割り算～÷～)
+# 計算の問題出題(掛け算～✖～)
 if 'ii' not in st.session_state:
     st.session_state.ii = ran.randint(1, 100)
     st.session_state.jj = ran.randint(1, 100)
 
-if st.button('計算の問題出題(割り算～÷～)'):
+if st.button('計算の問題出題(掛け算～✖～)'):
     st.session_state.ii = ran.randint(1, 100)
     st.session_state.jj = ran.randint(1, 100)
 
-st.write(st.session_state.ii, '÷', st.session_state.jj)
+st.write(st.session_state.ii, '✖', st.session_state.jj)
+
+if st.button('こたえ (掛け算)'):
+    st.session_state.ancerf = st.session_state.ii * st.session_state.jj
+    st.write(st.session_state.ancerf)
+
+# 計算の問題出題(割り算～÷～)
+if 'kk' not in st.session_state:
+    st.session_state.kk = ran.randint(1, 100)
+    st.session_state.ll = ran.randint(1, 100)
+
+if st.button('計算の問題出題(割り算～÷～)'):
+    st.session_state.kk = ran.randint(1, 100)
+    st.session_state.ll = ran.randint(1, 100)
+
+st.write(st.session_state.kk, '÷', st.session_state.ll)
 
 if st.button('こたえ (割り算)'):
-    st.session_state.ancerf = round(st.session_state.ii / st.session_state.jj, 10)
-    st.write(st.session_state.ancerf)
+    st.session_state.ancerg = round(st.session_state.kk / st.session_state.ll, 10)
+    st.write(st.session_state.ancerg)
