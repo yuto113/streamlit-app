bedeec2fe42dbaa83ef840d03c4496194a761f6f
yuto113 <yuutonishie@gmail.com>
2025-02-19 06:57:15 +0900

    ver?

--- app--.py
+++ app--.py
@@ -5,3 +5,6 @@ st.write('アップデートをしています。')
 st.write('現在使用不可')
 st.write('以下のサイトをご利用ください。')
 st.write('https://app-2024-5blue-yuuto.streamlit.app')
+
+st.write('このプログラムを作る際に関係した方々の詳しい情報(個人情報は、おおよそありません。)')
+st.write('https://app-d0t-1ab0.streamlit.app')
