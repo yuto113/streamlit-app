f083f99db5360661b80d10b9b43cd48ac6990a1f
yuto113 <yuutonishie@gmail.com>
2025-02-18 21:06:51 +0900

    ver?

---
+++ Dot.labo.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.write('こちらは、「Dot.labo(「ロボ団」の教材+「dot.labo」の教材を使ったプログラミング教室)で学んだことを生かしてアプリを作成しています。')
+st.write('また、プログラムは、「python言語」の「streamlit」という機能を使用しています。')
+st.write('是非お使いください。ロボ団または、dot.laboの公式サイトは以下のリンクからお願いします。')
+st.write('以下は、Dot.laboの公式サイトです。')
+st.write('https://dot-labo.jp/')
+st.write('以下は、ロボ団の公式サイトです。')
+st.write('https://robo-done.com/')
